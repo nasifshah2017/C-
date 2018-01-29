b94cdbeb1d20069bbe425b7e7284fde291efa9b5
nasifshah2017 <33353457+nasifshah2017@users.noreply.github.com>
2018-01-28 20:08:13 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,84 +1,79 @@
+/*File: Lab 04.cpp
+ *----------------------------------
+ * Name:    Syed Nasif Ali (201304650)
+ * Username: x2013ijn
+ * Course:   CSCI 161
+ * Purpose:  Lab #4
+ * Date: 2015/10/08
+ * This program is to help customers in a departmental store which inputs the prices of each items purchased by the customers and shows the total amount.
+   Then it asks the amount they want to pay and shows the amount of change they will receive from the cashier.
+   */
+
+
 #include <iostream>
 #include <iomanip>
-#include <ctime>
-#include <windows.h>
 
 using namespace std;
 
-
-
-
 int main()
-{
-    time_t now;
-    tm     current, event;
-    int InputDay, InputHour, InputMinute, InputSecond, InputYear, InputMonth;
-    int Hour, Minute, Second;
-    double seconds, Day;
 
-    cout << " Please enter the year of the event. (Above 1900) \n";
-    cin  >> InputYear;
-    cout << "Please enter the month of the event. \n";
-    cin  >> InputMonth;
-    cout << " Please enter the day of the event. \n";
-    cin  >> InputDay;
-    cout << "Please enter the hour of the event for 24hrs clock \n";
-    cin  >> InputHour;
-    cout << " Please enter the minute of the event \n";
-    cin  >> InputMinute;
-    cout << " Please enter the second of the event \n";
-    cin  >> InputSecond;
+{    int dollars, quarters, cents;
+    double value, sum, total, amount, change, shot;
 
-    /* Set the time for X-Ring ceremony Dec 3, 2015 at 2 pm
-       hour - 24 hour clock    mon - from 0 to 11   year - counts from 1900 */
+    cout << setprecision (2) << fixed;
+    cout << " Please enter the prices of your items one by one. Enter 0 when you are done. \n";
 
-    event.tm_hour = InputHour +1; event.tm_min = InputMinute; event.tm_sec = InputSecond;
-    event.tm_mday = InputDay;  event.tm_mon = InputMonth -1; event.tm_year = InputYear - 1900;
+    sum = 0;
 
-    while (true)                                 // infinite loop
+
+    cin >> value;
+
+
+     while (value > 0)
     {
-        now = time(NULL);                        // get current time
 
-        current = *localtime(&now);              // convert to usable form
-
-        seconds = difftime(mktime(&event), now); // seconds until x-ring
-
-        cout << "\nCOUNTDOWN TO WORLD CUP\n\n\n";
-
-        cout << setfill('0') << fixed << setprecision(0);
-
-        cout << setw(2) << current.tm_hour << ':'
-             << setw(2) << current.tm_min  << ':'
-             << setw(2) << current.tm_sec  << ' '
-             << setw(2) << current.tm_mday << '/'
-             << setw(2) << current.tm_mon + 1 << '/'
-             << setw(4) << 1900 + current.tm_year << "  CURRENT TIME" << endl;
-
-        cout << "\n\n" << setw(19) << seconds << "  seconds\n\n\n";
-
-        Day = (seconds/86400);
-        Hour = (((int)seconds/3600)%24);
-        Minute = (((int)seconds/60)%60);
-        Second = ((int)seconds%60);
-
-        cout << (int)Day << " days  "
-             << Hour <<  ":"
-             << Minute <<  ":"
-             << Second << "\n\n";
-
-        cout << setw(2) << event.tm_hour << ':'
-             << setw(2) << event.tm_min  << ':'
-             << setw(2) << event.tm_sec  << ' '
-             << setw(2) << event.tm_mday << '/'
-             << setw(2) << event.tm_mon + 1 << '/'
-             << setw(4) << 1900 + event.tm_year << "  EVENT" << endl << endl;
-
-        Sleep(1000);
-        system("cls");
-
-
+    sum = sum + value;
+    cin >> value;
 
     }
+    total = (sum * .15) + sum;
+
+
+    cout << " Your total bill is " << total << " after including HST \n";
+    cout << " Please enter the amount you paid \n";
+    cin >> amount;
+
+    if (amount < total)
+    {
+      shot = (total - amount);
+
+      cout << " Sorry !, you are $ " << shot << " short from your total \n";
+
+    }
+
+   if (amount == total)
+   {
+      cout << " You have no change to receive. Thank you for shopping in Walmart. Please visit us again. \n";
+   }
+
+   if (amount > total)
+   {
+       change = (amount - total);
+
+       dollars = change;
+
+       cents = (change - dollars) * 100;
+
+       quarters = cents / 25;
+       cents = cents % 25;
+
+       cout << " You have $ " << dollars << " and " << quarters << " quarters and " << cents << " cents change to receive \n";
+       cout << " Thank you for shopping with Walmart. Please come back again. \n";
+       cout << " Happy Halloween !!! \n";
+   }
+
+
+
 
 
 
