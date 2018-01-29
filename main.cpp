4a84e919e6356a204eda6773b558fa985cd3e748
nasifshah2017 <33353457+nasifshah2017@users.noreply.github.com>
2018-01-28 20:08:55 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,79 +1,106 @@
-/*File: Lab 04.cpp
- *----------------------------------
- * Name:    Syed Nasif Ali (201304650)
- * Username: x2013ijn
- * Course:   CSCI 161
- * Purpose:  Lab #4
- * Date: 2015/10/08
- * This program is to help customers in a departmental store which inputs the prices of each items purchased by the customers and shows the total amount.
-   Then it asks the amount they want to pay and shows the amount of change they will receive from the cashier.
-   */
-
+// File: Lab 05.cpp
+// Name: Syed Nasif Ali Shah (201304650)
+// Username: x201304650
+// Course: CSCI 161
+// Purpose: Lab 5
+// Date: 2015/10/16
+// This program tells the user are they driving at the safe speed. If they are not how much will they be fined.
 
 #include <iostream>
-#include <iomanip>
 
 using namespace std;
 
 int main()
+{
+    int limit, AreaZone;
+    double velocity, LimitCross, fine;
 
-{    int dollars, quarters, cents;
-    double value, sum, total, amount, change, shot;
+    cout << " Please enter the speed limit. \n";
+    cin >> limit;
+    cout << " Please enter the speed of your vehicle. \n";
+    cin >> velocity;
 
-    cout << setprecision (2) << fixed;
-    cout << " Please enter the prices of your items one by one. Enter 0 when you are done. \n";
+    LimitCross = velocity - limit;
 
-    sum = 0;
-
-
-    cin >> value;
-
-
-     while (value > 0)
+    if ( LimitCross < 0)
     {
+        LimitCross = -1 * LimitCross;
 
-    sum = sum + value;
-    cin >> value;
-
-    }
-    total = (sum * .15) + sum;
-
-
-    cout << " Your total bill is " << total << " after including HST \n";
-    cout << " Please enter the amount you paid \n";
-    cin >> amount;
-
-    if (amount < total)
-    {
-      shot = (total - amount);
-
-      cout << " Sorry !, you are $ " << shot << " short from your total \n";
-
+        cout << " You are "
+             << LimitCross
+             << " km/hr below the speed limit and safe to go with it."
+             << " Don't forget to apply black pepper on your turkey \n";
+        return 0;
     }
 
-   if (amount == total)
-   {
-      cout << " You have no change to receive. Thank you for shopping in Walmart. Please visit us again. \n";
-   }
+    if ( 1 <= LimitCross && LimitCross <= 15)
+    {
+        fine = 227.41;
+    }
 
-   if (amount > total)
-   {
-       change = (amount - total);
+    if ( 16 <= LimitCross && LimitCross <= 30 )
+    {
+        fine = 284.91;
+    }
 
-       dollars = change;
+    if ( 31 <= LimitCross && LimitCross <= 50)
+    {
+        fine = 399.91; // what would happen if the speed is 50.5 km/hr?
+    }
 
-       cents = (change - dollars) * 100;
+    if (LimitCross >= 51)
+    {
+        fine = 2421.41;
+    }
 
-       quarters = cents / 25;
-       cents = cents % 25;
+   cout << " You have crossed your limit by "
+        << LimitCross
+        << " km/hr If you are driving at a regular zone \n"
+        << "then your fine is $"
+        << fine
+        << " \n";
 
-       cout << " You have $ " << dollars << " and " << quarters << " quarters and " << cents << " cents change to receive \n";
-       cout << " Thank you for shopping with Walmart. Please come back again. \n";
-       cout << " Happy Halloween !!! \n";
-   }
+   cout << " If you are driving at a school or a construction zone"
+        << " then press 2"
+        << " if you are \n"
+        << " at both then press 4"
+        << " if you are not at any one of them then press 0 or any key  "
+        << " to continue \n";
 
+    cin >> AreaZone;
 
+    if ( AreaZone == 2)
+    {
+        fine = 2 * fine;
 
+        cout << " Sorry but your fine is now $"
+             << fine
+             << " Please lower your speed by "
+             << LimitCross
+             << " km/hr to save \n"
+             << " yourself for the turkey \n";
+
+    }
+    else if ( AreaZone == 4)
+    {
+        fine = 4 * fine;
+
+        cout << " Sorry buddy its ain't your day"
+             << " You are gonna fined by $"
+             << fine
+             << " If you want to\n"
+             << " cheer your turkey"
+             << " then lower you speed by "
+             <<  LimitCross
+             << " km/hr \n";
+    }
+
+    else
+    {
+        cout << " Lower your speed and drive safe,"
+             << " And don't forget to apply black pepper"
+             << " on your turkey \n";
+    }
 
 
 
