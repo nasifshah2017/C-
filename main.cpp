bf7edb3397c3f134f8a247ac81a7c64d66aebe12
nasifshah2017 <33353457+nasifshah2017@users.noreply.github.com>
2018-01-28 20:04:08 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,6 +4,38 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+   int people, secondDays, days;
+   double  SignUpRate;
+
+   people = 1;
+
+   days = 0;
+
+   cout << " Please enter the number of people signed up \n";
+   cin >> people;
+
+   for ( people = 1; people < 1250; people++)
+   {
+       SignUpRate = .05 * people;
+
+       days = days + 1;
+
+       switch (people)
+       {
+           case 500:
+           cout << " It took " << days << " days to reach 500 people \n";
+           secondDays = days * 2;
+           cout << " It took " << secondDays << " to reach 1000 people \n";
+           default:
+            cout << "";
+       }
+   }
+
+   cout << days << "/n";
+
+
+
+
+
     return 0;
 }
